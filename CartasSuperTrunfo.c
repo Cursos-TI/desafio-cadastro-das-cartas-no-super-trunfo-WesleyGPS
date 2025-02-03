b0f3ee5a041bcd228242a26c5d7bec07b96fec3c
WesleyGPS <wesleygode73@gmail.com>
2025-02-03 21:16:56 +0000

    Confirmação de entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,13 +10,40 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    // Variaveis
+    char codigo_cidade[50], nome[50];
+    int populacao, pib, num_pontos_turisticos;
+    float area;
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    // Coleta de dados
+    printf("Codigo da Cidade: ");
+    scanf("%s", &codigo_cidade);
+    printf("Codigo da Cidade: ");
+    scanf("%s", &nome);
+    printf("Codigo da Cidade: ");
+    scanf("%d", &populacao);
+    printf("Codigo da Cidade: ");
+    scanf("%f", &area);
+    printf("Codigo da Cidade: ");
+    scanf("%d", &pib);
+    printf("Codigo da Cidade: ");
+    scanf("%d", &num_pontos_turisticos);
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    // Exibindo dados
+    printf("Codigo da Cidade: %");
+    printf("Nome: ");
+    printf("População: ");
+    printf("Área: ");
+    printf("PIB: ");
+    printf("Número de pontos turísticos: ");
 
     return 0;
 }
